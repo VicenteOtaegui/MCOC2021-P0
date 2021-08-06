02c5dda3b8dad3e55591df3b605987682ebe6d3e
VicenteOtaegui <vaotaegui@miuandes.cl>
2021-08-05 22:49:44 -0400

    casi listo casi casi

---
+++ Graficar.py
@@ -0,0 +1,72 @@
+import matplotlib.pylab as plt
+from psutil import virtual_memory
+
+# print (virtual_memory().total/(10**9))
+
+Ns = []
+dts = []
+mems = []
+
+for i in range (10):
+	fid = open(f"rendimiento{i}.txt","r")
+	L=0
+	for line in fid:
+		sl = line.split()
+		N = int(sl[2])
+		dt = float(sl[5])
+		mem = int(sl[9])
+		L+=1
+		Ns.append(N)
+		dts.append(dt)
+		mems.append(mem)
+
+		#print (sl)
+
+	fid.close()
+
+# print (Ns)
+# print (dts)
+# print (mems)
+
+## xticks ##
+labels_x = ["10","20","50","100","200","500", "1000","2000","5000","10000","20000"]
+Ns1 = [10,20,50,100,200,500,1000,2000,5000,10000,20000]
+
+## yticks tiempo transcurrido ##
+labels_y1 = ["0.1 ms", "1 ms", "10 ms", "0.1 s", "1 s", "10 s", "1 min", "10 min"]
+dts1 = [10**(-4), 10**(-3), 10**(-2), 10**(-1), 10**(0), 10, 60, 600]
+
+## yticks uso memoria ##
+labels_y2 = ["1 KB", "10 KB", "100 KB", "1 MB", "10 MB", "100 MB", "1 GB", "10 GB"]
+mems1 = [1e3, 10**(4), 10**(5), 10**(6), 10**(7), 10**(8), 10**(9), 10**(10)]
+
+
+plt.figure(1)
+
+plt.subplot(2,1,1)
+
+plt.ylabel("Tiempo transcurrido")
+plt.title("Rendimiento A@B")
+
+for i in range(L):
+	plt.loglog(Ns[i*L:(i+1)*L],dts[i*L:(i+1)*L],marker=".",linestyle='-.')
+
+plt.xticks(Ns1, labels_x, rotation = 45)
+plt.yticks(dts1, labels_y1)
+plt.grid(True)
+plt.xticks(visible=False)
+
+plt.subplot(2,1,2)
+
+plt.xlabel("Tamaño matriz N")
+plt.ylabel("Uso memoria")
+plt.loglog(Ns,mems,marker=".")
+plt.xticks(Ns1, labels_x, rotation = 45)
+plt.yticks(mems1, labels_y2)
+plt.grid(True)
+plt.axhline(y=12e9, color='k', linestyle='--')
+
+plt.tight_layout()
+plt.savefig("plot.png")
+
+plt.show()
--- timing_matmul.py
+++ timing_matmul.py
@@ -1,17 +1,60 @@
-from numpy import zeros
+from numpy import zeros, float16, float32, float64
 from time import perf_counter
+import matplotlib.pylab as plt
 
-N = 2000
-A = zeros ((N, N))+1
-B = zeros ((N, N))+2
 
-#print (f"A = {A}")
-#print (f"B = {B}")
+Ns = [1,5,10]
+for i in range (20,600,20):
+	Ns.append(i)
+Ns[-1]=8000
+Ns[-2]=5000
+Ns[-3]=2000
+Ns[-4]=1000
 
-t1 = perf_counter()
-C = A@B
-t2 = perf_counter()
 
-dt = t2-t1
+#print (Ns)
+#print (len(Ns))
+# exit(0)
 
-print (f"dt = {dt} s")
+dts = []
+mems = []
+
+
+for i in range(10):
+	fid = open(f"rendimiento{i}.txt", "w")
+
+	for N in Ns:
+
+		uso_memoria = 0
+
+		A = zeros ((N, N), dtype=float16) + 1
+
+		uso_memoria_teorico = 4*N*N #bytes... float32 4bytes
+		uso_memoria_numpy  = A.nbytes
+		#print (f"uso_memoria_teorico = {uso_memoria_teorico}")
+		#print (f"uso_memoria_numpy = {uso_memoria_numpy}")
+
+		B = zeros ((N, N))+2
+
+		# print (f"A = {A}")
+		# print (f"B = {B}")
+
+		t1 = perf_counter()
+		C = A@B
+		t2 = perf_counter()
+
+		uso_memoria_total = A.nbytes + B.nbytes + C.nbytes
+
+		dt = t2-t1
+		dts.append(dt)
+		mems.append(uso_memoria_total)
+
+		print (f"N = {N} dt = {dt} s _ uso_memoria_total= {uso_memoria_total} bytes _ flops = {N**3/dt} flops/s")
+
+		fid.write(f"N = {N} dt = {dt} s _ uso_memoria_total= {uso_memoria_total} bytes \n" )
+	
+	print (f"iteración = {i}")
+
+	fid.close()
+
+### END ###
